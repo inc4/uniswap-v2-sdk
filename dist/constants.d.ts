8d4869b5e10ad9e416bb109093e05ddeef8ba17e
Maksim Neroda <maksimneroda@Maksims-MacBook-Air.local>
2021-10-29 17:39:30 +0300

    Initial commit

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x0B306BF915C4d645ff596e518fAf3F9669b97016";
+export declare const INIT_CODE_HASH = "0x134118ad020be07822b46af62ddeafc66016697db73182ad42551f805789cc3a";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
